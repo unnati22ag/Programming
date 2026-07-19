6de038b109f1a9b2ec1b9056667ff505aa374a55
unnati22ag <unnatiagrawal2006@gmail.com>
2026-07-19 19:06:32 +0530

    Add files via upload

---
+++ fibonnacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main(){
+int a;
+printf("enter  the age \n");
+scanf("%d",&a);
+if(a>=18){
+    printf("you can vote");
+}
+else{
+    printf("you cannot vote");
+}
+getch();
+}
+
